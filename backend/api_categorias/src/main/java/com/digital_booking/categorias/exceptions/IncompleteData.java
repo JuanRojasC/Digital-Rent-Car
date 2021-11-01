9ec59462b400f714ab76511e20211ea4a31f3971
JuanRojasC <appsjuanrojas@gmail.com>
2021-11-01 18:10:45 -0500

    Clean Directory

--- backend/api_categorias/src/main/java/com/digital_booking/categorias/exceptions/IncompleteData.java
+++
@@ -1,7 +0,0 @@
-package com.digital_booking.categorias.exceptions;
-
-public class IncompleteData extends Exception{
-    public IncompleteData(String message) {
-        super(message);
-    }
-}
--- frontend/src/pages/Registro/Registro.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Signin from '../../components/Signin/Signin';
-
-import './registro.css';
-
-export default function Registro() {
-    return <Signin/>
-}
